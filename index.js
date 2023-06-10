80ea35ba4e3a03e5ad576a3d14a555f280658584
Harsh Singh <sujeetsingh734321@gmail.com>
2023-06-10 15:43:13 +0530

    dfjdsfhsjd

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
