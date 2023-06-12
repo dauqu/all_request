b4cf94386bac992fb5b75e7fe7c8473ea464a4bf
Harsh Singh <sujeetsingh734321@gmail.com>
2023-06-12 15:08:14 +0530

    gfdgfd

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 
+//Allow cors
+const cors = require("cors");
+//Loop of allowed origins
+const allowedOrigins = [
+  "http://localhost:3001",
+  "http://localhost:3000",
+  "https://admin-for-all.vercel.app",
+  "https://dauqunews.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+//Connect to database
+const connectDB = require("./config/database");
+connectDB();
+
 // Allow express to use json
 app.use(bodyParser.json({ limit: "10mb" }));
 
@@ -10,6 +31,9 @@ app.get("/", (req, res) => {
     console.log(req.body);
     res.send("Hello World!");
 });
+
+app.use("/sms", require("./routes/sms"));
+app.use("/login", require("./routes/login"));
 
 app.post("/", (req, res) => {
    //Print all data 
---
+++ routes/login.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const LoginSchema = require("./../models/login_schema");
+
+//Get all watchlists
+router.get("/", async (req, res) => {
+  try {
+    const login = await LoginSchema.find();
+    res.status(200).json(login);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get one watchlist
+router.get("/:id", async (req, res) => {
+  try {
+    const watchlist = await LoginSchema.findById(req.params.id).lean();
+    if (!watchlist) {
+      return res
+        .status(404)
+        .json({ message: "Watchlist not found", status: "error" });
+    }
+    res.status(200).json(watchlist);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Create One
+router.post("/", async (req, res) => {
+  const login = new LoginSchema({
+    user_id: req.body.user_id,
+    phone: req.body.phone,
+    password: req.body.password,
+  });
+  try {
+    const newWatchlist = await login.save();
+    res.status(201).json(newWatchlist);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Delete one watchlist
+router.delete("/:id", async (req, res) => {
+  try {
+    const watchlist = await LoginSchema.findById(req.params.id);
+    if (!watchlist) {
+      return res
+        .status(404)
+        .json({ message: "Watchlist not found", status: "error" });
+    }
+    await LoginSchema.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Watchlist deleted", status: "success" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
